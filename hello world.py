75ae7404a22a4fa6b80e76d2df37b09e0fd70362
Om Gupta <omg04055@gmail.com>
2020-06-05 21:38:17 +0530

    google change 1

--- hello world.py
+++ hello world.py
@@ -2,4 +2,30 @@ Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 23:03:10) [MSC v.1916 64 bit (AM
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> print ("helloworld")
 helloworld
+>>> print ("change 1")
+change 1
+>>> def change
+SyntaxError: invalid syntax
+>>> def change:
+	
+SyntaxError: invalid syntax
+>>> def change()
+SyntaxError: invalid syntax
+>>> def change():
+	print("change/n")
+
+	
+>>> change()
+change/n
+>>>  def change():
+	print("change\n")
+	
+SyntaxError: unexpected indent
+>>> def change():
+	print("change\n")
+
+	
+>>> change()
+change
+
 >>> 
